1186d6c8d83f492ff32f0570eaf6ed2d0f24fdd9
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-14 09:37:59 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Calendar;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Datum och tid:");
+        System.out.println(Calendar.getInstance().getTime().toString());
+    }
+}
